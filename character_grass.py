c65551840cbb1105bcf8fd0715ce72f5d80e0cf7
Kangdahyeon <kdhkdh6345@naver.com>
2024-09-24 11:06:39 +0900

    rectangle 운동 x값 조정

--- character_grass.py
+++ character_grass.py
@@ -16,12 +16,12 @@ def draw_boy(x,y):
     
 def run_top():
     print('top')
-    for x in range(20,750,10):
+    for x in range(50,750,10):
         draw_boy(x,550)
     pass
 def run_bottom():
     print('bottom')
-    for x in range(750,20,-10):
+    for x in range(750,50,-10):
         draw_boy(x,50)
     pass
 def run_right():
@@ -32,7 +32,7 @@ def run_right():
 def run_left():
     print('left')
     for y in range(0,550,10):
-        draw_boy(0,y)
+        draw_boy(50,y)
     pass
 
 def run_rectangle():
