a44da4c1eb14c16aacab742a6a97c74f5df2202a
Kangdahyeon <kdhkdh6345@naver.com>
2024-09-24 10:21:57 +0900

    원 운동 그리기 완료

--- character_grass.py
+++ character_grass.py
@@ -11,16 +11,24 @@ character = load_image('character.png')
 
 def run_rectangle():
     print('RECTANGLE')
-    clear_canvas_now()
-    character.draw_now(400,300)
-    delay(1)
-    pass
-def run_circle():
-    print('CIRCLE')
     
     clear_canvas_now()
     character.draw_now(400,300)
     delay(1)
+    pass
+
+def run_circle():
+    print('CIRCLE')
+    
+    r,cx,cy=300,800//2,600//2
+   
+    for d in range(0,360):
+        x=r*math.cos(math.radians(d))+cx
+        y=r*math.sin(math.radians(d))+cy
+    
+        clear_canvas_now()
+        character.draw_now(x,y)
+        delay(0.01)
     pass
 
 
